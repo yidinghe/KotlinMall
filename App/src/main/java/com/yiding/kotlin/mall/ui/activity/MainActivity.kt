6cd1845baa11a069769c0416388857d2867b827c
Yiding He <yidinghe2012@gmail.com>
2018-07-24 20:22:20 -0400

    Initial commit

---
+++ App/src/main/java/com/yiding/kotlin/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.yiding.kotlin.mall.ui.activity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.yiding.kotlin.mall.R
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
