366a9ff7fb3acc4f38bad4c470fc73909ce118e1
Yiding He <yidinghe2012@gmail.com>
2018-07-26 21:00:40 -0400

    Added UserCenter Module.

---
+++ UserCenter/src/main/java/com/yiding/kotlin/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.yiding.kotlin.user.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.yiding.kotlin.user.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+        setContentView(R.layout.activity_register)
+    }
+}
