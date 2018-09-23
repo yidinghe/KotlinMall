b2d6dfbe45fa906f663e4aa47e5a24c72c75cc18
Yiding He <yidinghe2012@gmail.com>
2018-09-23 17:21:57 -0400

    Fixed the permission issue.

--- UserCenter/src/main/java/com/yiding/kotlin/user/ui/activity/UserInfoActivity.kt
+++ UserCenter/src/main/java/com/yiding/kotlin/user/ui/activity/UserInfoActivity.kt
@@ -1,11 +1,25 @@
 package com.yiding.kotlin.user.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.View
 import com.bigkoo.alertview.AlertView
 import com.bigkoo.alertview.OnItemClickListener
+import com.jph.takephoto.app.TakePhoto
+import com.jph.takephoto.app.TakePhotoImpl
+import com.jph.takephoto.compress.CompressConfig
+import com.jph.takephoto.model.InvokeParam
+import com.jph.takephoto.model.TContextWrap
+import com.jph.takephoto.model.TResult
+import com.jph.takephoto.permission.InvokeListener
+import com.jph.takephoto.permission.PermissionManager
+import com.jph.takephoto.permission.TakePhotoInvocationHandler
 import com.yiding.kotlin.base.ext.onClick
 import com.yiding.kotlin.base.ui.activity.BaseMvpActivity
+import com.yiding.kotlin.base.utils.DateUtils
 import com.yiding.kotlin.user.R
 import com.yiding.kotlin.user.injection.component.DaggerUserComponent
 import com.yiding.kotlin.user.injection.module.UserModule
@@ -13,14 +27,24 @@ import com.yiding.kotlin.user.presenter.UserInfoPresenter
 import com.yiding.kotlin.user.presenter.view.UserInfoView
 import kotlinx.android.synthetic.main.activity_user_info.*
 import org.jetbrains.anko.toast
+import java.io.File
 
-class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
+class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView,
+    TakePhoto.TakeResultListener, InvokeListener {
+
+
+    private lateinit var mTakePhoto: TakePhoto
+    private lateinit var mTempFile: File
+    private lateinit var mInvokeParam: InvokeParam
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
 
+        mTakePhoto =
+                TakePhotoInvocationHandler.of(this).bind(TakePhotoImpl(this, this)) as TakePhoto
         initView()
+        mTakePhoto.onCreate(savedInstanceState)
     }
 
     private fun initView() {
@@ -39,9 +63,13 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
             this,
             AlertView.Style.ActionSheet,
             OnItemClickListener { o, position ->
+                mTakePhoto.onEnableCompress(CompressConfig.ofDefaultConfig(), false)
                 when (position) {
-                    0 -> toast("拍照")
-                    1 -> toast("相册")
+                    0 -> {
+                        createTempFile()
+                        mTakePhoto.onPickFromCapture(Uri.fromFile(mTempFile))
+                    }
+                    1 -> mTakePhoto.onPickFromGallery()
                 }
             }).show()
     }
@@ -52,4 +80,49 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
         mPresenter.mView = this
     }
 
+    override fun takeSuccess(result: TResult?) {
+        Log.d("takePhoto", result?.image?.originalPath)
+        Log.d("takePhoto", result?.image?.compressPath)
+    }
+
+    override fun takeCancel() {
+    }
+
+    override fun takeFail(result: TResult?, msg: String?) {
+        Log.e("takePhoto", msg)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        mTakePhoto.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val permissionType =
+            PermissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionManager.handlePermissionsResult(this, permissionType, mInvokeParam, this)
+    }
+
+    override fun invoke(invokeParam: InvokeParam): PermissionManager.TPermissionType {
+        val permissionType =
+            PermissionManager.checkPermission(TContextWrap.of(this), invokeParam.method)
+        if (PermissionManager.TPermissionType.WAIT == permissionType) {
+            mInvokeParam = invokeParam
+        }
+        return permissionType
+    }
+
+    fun createTempFile() {
+        val tempFileName = "${DateUtils.curTime}.png"
+        if (Environment.MEDIA_MOUNTED == Environment.getExternalStorageState()) {
+            mTempFile = File(Environment.getExternalStorageDirectory(), tempFileName)
+            return
+        }
+        mTempFile = File(filesDir, tempFileName)
+    }
 }
